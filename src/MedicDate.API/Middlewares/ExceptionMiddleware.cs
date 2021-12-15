c70dad219f456c5248f1a5f1282eb84dbd511364
nelsonmarro99@outlook.com <nelsonmarro99@outlook.com>
2021-12-15 12:15:41 -0500

    ok

--- src/MedicDate.API/Middlewares/ExceptionMiddleware.cs
+++ src/MedicDate.API/Middlewares/ExceptionMiddleware.cs
@@ -6,41 +6,41 @@ namespace MedicDate.API.Middlewares;
 
 public class ExceptionMiddleware
 {
-   private readonly IHostEnvironment _env;
-   private readonly ILogger _logger;
-   private readonly RequestDelegate _next;
+    private readonly IHostEnvironment _env;
+    private readonly ILogger _logger;
+    private readonly RequestDelegate _next;
 
-   public ExceptionMiddleware(RequestDelegate next,
-     ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
-   {
-      _next = next;
-      _logger = logger;
-      _env = env;
-   }
+    public ExceptionMiddleware(RequestDelegate next,
+      ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+    {
+        _next = next;
+        _logger = logger;
+        _env = env;
+    }
 
-   public async Task InvokeAsync(HttpContext context)
-   {
-      try
-      {
-         await _next(context);
-      }
-      catch (Exception ex)
-      {
-         _logger.LogError(ex, ex.Message);
-         context.Response.ContentType = "application/json";
-         context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, ex.Message);
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
 
-         var response = _env.IsDevelopment()
-           ? new ApiExceptionDto(context.Response.StatusCode, ex.Message,
-             ex.StackTrace)
-           : new ApiExceptionDto(context.Response.StatusCode,
-             "Internal Server Error");
+            var response = _env.IsDevelopment()
+              ? new ApiExceptionDto(context.Response.StatusCode, ex.Message,
+                ex.StackTrace)
+              : new ApiExceptionDto(context.Response.StatusCode,
+                "Internal Server Error");
 
-         var options = new JsonSerializerOptions
-         { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+            var options = new JsonSerializerOptions
+            { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
 
-         var json = JsonSerializer.Serialize(response, options);
-         await context.Response.WriteAsync(json);
-      }
-   }
+            var json = JsonSerializer.Serialize(response, options);
+            await context.Response.WriteAsync(json);
+        }
+    }
 }
